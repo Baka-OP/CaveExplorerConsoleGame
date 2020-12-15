7bbedf729238f647a50150d38d4d1594efd66589
YoriMirus <misawalach00@seznam.cz>
2020-12-15 16:01:05 +0100

    minor bug fix + automatically resizing to fullscreen when starting

--- Space cave expedition/Program.cs
+++ Space cave expedition/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
 using Space_cave_expedition.Models;
 using Space_cave_expedition.Enums;
@@ -8,15 +9,31 @@ namespace Space_cave_expedition
 {
     class Program
     {
-        static string ProgramLocation = Environment.CurrentDirectory;
-        static void Main(string[] args)
+        /// <summary>
+        /// If the player resizes the console, the cursor becomes visible again, this will make it invisible again every 1 second. Run this on a paralel thread, or else the whole main thread will be blocked by thread.sleep.
+        /// </summary>
+        static void CursorVisibilitySetter()
         {
+            while (true)
+            {
+                Console.CursorVisible = false;
+                Thread.Sleep(1000);
+            }
+        }
+
+        static readonly string ProgramLocation = Environment.CurrentDirectory;
+        static void Main()
+        {
+            ThreadStart CursorVisibilitySetterMethod = CursorVisibilitySetter;
+            Thread CursorVisibilitySetterThread = new Thread(CursorVisibilitySetterMethod);
+            CursorVisibilitySetterThread.Start();
+
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
-            Console.CursorVisible = false;
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
 
             Player player = new Player(0, 2);
-
             Map testMap = new Map(File.ReadAllText(ProgramLocation + "\\Map Templates\\TestMap.txt"), ConsoleColor.Black, ConsoleColor.Gray, player);
             testMap.Start();
 
